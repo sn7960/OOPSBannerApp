f3fa64304c1331e926acfb6b14444c6cc4404f7f
sn7960 <sn7960@srmist.edu.in>
2026-03-02 01:00:09 +0530

    UC3: OOPSBannerApp using String.join()

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,15 +1,11 @@
-/*
-OOPSBannerApp
-UC1 -  Print OOPS to console
- */
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("   ***     " + "   ***     " + "  *****    " + "    *****   ");
-        System.out.println(" **   **   " + " **   **   " + "  **   **   " + " **        ");
-        System.out.println("**     **  " + "**     **  " + "  **   **   " + "**        ");
-        System.out.println("**     **  " + "**     **  " + "  *****     " + "  ******    ");
-        System.out.println("**     **  " + "**     **  " + "  **        " + "        **  ");
-        System.out.println(" **   **   " + " **   **   " + "  **        " + "       **  ");
-        System.out.println("   ***     " + "   ***     " + "  **        " + "  *****    ");
+        System.out.println(String.join(" ","   ***     " , "   ***     " , "  *****     " ,"   *****   "));
+        System.out.println(String.join(" "," **   **   " , " **   **   " , "  **   **   " ," **        "));
+        System.out.println(String.join(" ","**     **  " , "**     **  " , "  **   **   " ,"  **        "));
+        System.out.println(String.join(" ","**     **  " , "**     **  " , "  *****     " ,"    ***    "));
+        System.out.println(String.join(" ","**     **  " , "**     **  " , "  **        " ,"        **  "));
+        System.out.println(String.join(" "," **   **   " , " **   **   " , "  **        " ,"       **    "));
+        System.out.println(String.join(" ","   ***     " , "   ***     " , "  **        " ,"  *****    "));
     }
 }
