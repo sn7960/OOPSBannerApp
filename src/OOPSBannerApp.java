037a1ee5227cfe04d45550ccdd1df54b6055bfab
sn7960 <sn7960@srmist.edu.in>
2026-03-02 06:23:32 +0530

    UC6:Assignment - Static Functions

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,16 +1,61 @@
 public class OOPSBannerApp {
-    public static void main(String[] args) {
-        String[] line = {
-                String.join(" ", "   ***     ", "   ***     ", "  *****     ", "   *****   "),
-                String.join(" ", " **   **   ", " **   **   ", "  **   **   ", " **        "),
-                String.join(" ", "**     **  ", "**     **  ", "  **   **   ", "  **        "),
-                String.join(" ", "**     **  ", "**     **  ", "  *****     ", "    ***    "),
-                String.join(" ", "**     **  ", "**     **  ", "  **        ", "        **  "),
-                String.join(" ", " **   **   ", " **   **   ", "  **        ", "       **    "),
-                String.join(" ", "   ***     ", "   ***     ", "  **        ", "  *****    ")
-        };
-        for (String lines : line) {
-            System.out.println(lines);
+            // Method to generate the pattern for the letter 'O'
+            public static String[] getOPattern() {
+                return new String[]{
+                        "   ***    ",
+                        " **   **  ",
+                        "**      **",
+                        "**      **",
+                        "**      **",
+                        " **    ** ",
+                        "   ***    "
+                };
+            }
+
+            // Method to generate the pattern for the letter 'P'
+            public static String[] getPPattern() {
+                return new String[]{
+                        " *****   ",
+                        " **   ** ",
+                        " **   ** ",
+                        " *****   ",
+                        " **      ",
+                        " **      ",
+                        " **      "
+                };
+            }
+
+            // Method to generate the pattern for the letter 'S'
+            public static String[] getSPattern() {
+                return new String[]{
+                        "  *****  ",
+                        " **      ",
+                        " **      ",
+                        "  *****  ",
+                        "       **",
+                        "      ** ",
+                        " *****   "
+                };
+            }
+
+            // Main method to run the banner display
+            public static void main(String[] args) {
+
+                // Declare String arrays to hold patterns
+                String[] oPattern = getOPattern();
+                String[] pPattern = getPPattern();
+                String[] sPattern = getSPattern();
+
+                // Loop to assemble the word "OOPS"
+                for (int i = 0; i < oPattern.length; i++) {
+
+                    String line = String.join("  ",
+                            oPattern[i],
+                            oPattern[i],   // second O
+                            pPattern[i],
+                            sPattern[i]);
+
+                    System.out.println(line);
+                }
+            }
         }
-    }
-}
